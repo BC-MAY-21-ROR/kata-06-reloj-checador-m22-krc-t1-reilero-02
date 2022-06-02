cc3f76c09860b9f4979397c0f9f758255bee714b
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-06-02 18:45:33 -0500

    Added default value to true for employee's active attribute
    Co-authored-by: lucasvidela94 <lucas.videla994@gmail.com>
    Co-authored-by: guillermo-delprado <galfonso.delprado@gmail.com>
    Co-authored-by: HikingCarrot7 <ricardoibarra2044@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_233221) do
     t.integer "private_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "active"
+    t.boolean "active", default: true
   end
 
 end
