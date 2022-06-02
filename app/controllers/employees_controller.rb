7f8aa131bd051ad09b611833426dd23015ffa47a
guillermo-delprado <galfonso.delprado@gmail.com>
2022-06-02 15:49:34 -0500

    Modified employee controller and routes to add employees with a formulary and generate a list in the index page
    Co-authored-by: lucasvidela94 <lucas.videla994@gmail.com>
    Co-authored-by: HikingCarrot7 <ricardoibarra2044@gmail.com>

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,19 @@
 class EmployeesController < ApplicationController
-  def index; end
+  def index
+    @employees = Employee.all
+  end
 
-  def create; end
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create employee_params
+    @employee.save
+    redirect_to employees_path
+  end
+
+  def employee_params 
+    params.required(:employee).permit(:name, :email, :position, :employee_id, :private_number)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :employees
 
+  get 'employes/index'
+  get 'employes/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
