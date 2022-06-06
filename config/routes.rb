d02b21a795a628426dde83131a4af26624c3cca6
lucasvidela94 <69171039+lucasvidela94@users.noreply.github.com>
2022-06-06 18:43:04 -0300

    Create model for device autenthication
    Co-authored-by: lucasvidela94 <lucas.videla994@gmail.com>
    Co-authored-by: guillermo-delprado <galfonso.delprado@gmail.com>
    Co-authored-by: HikingCarrot7 <ricardoibarra2044@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :employees
   resources :employees
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_233221) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_212506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "branches", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -22,6 +29,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_233221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "active", default: true
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_employees_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
 
 end
