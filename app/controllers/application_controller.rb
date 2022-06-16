36c39234ab4ef98a45114d69b6a49bff8278b44e
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-06-16 16:30:23 -0500

    Fix relationship between users and attendances & check out logic
    Co-authored-by: lucasvidela94 <lucas.videla994@gmail.com>
    Co-authored-by: guillermo-delprado <galfonso.delprado@gmail.com>
    Co-authored-by: HikingCarrot7 <ricardoibarra2044@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  
+  def after_sign_in_path_for(resource)
+    if resource.role == 'admin'
+      admin_panel_path
+    else
+      root_path
+    end
+  end
+
+  def redirect_to_root(options = {})
+    redirect_to root_path, options
+  end
 end
